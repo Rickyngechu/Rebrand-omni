fd9fb2883f47f4009c65d314f1c8799a96c2eb62
Rickyngechu <ekngechu003@gmail.com>
2023-04-08 01:33:03 +0300

    updates

--- script.js
+++ script.js
@@ -444,37 +444,91 @@ const btnT = document.querySelectorAll('.btn-tab');
 const btn1 = document.querySelector('.btn-tab--1');
 const btn2 = document.querySelector('.btn-tab--2');
 
+const hero = document.querySelector('.section-hero');
+
 const cardP = document.querySelector('.price');
 const cardP1 = document.querySelector('.price-1');
 const cardP2 = document.querySelector('.price-2');
 const cardP3 = document.querySelector('.price-3');
 const headN = document.querySelector('.header');
 const navi = document.querySelector('.navigation');
-
+const priceBtn = document.querySelector('.navigation__link-btn');
+const heroDesc = document.querySelector('.hero__description');
+const hamOpen = document.querySelector('.hambuger');
+const mbClose = document.querySelector('.mb-close');
+const mobile = document.querySelector('.mb');
+const mobileCt = document.querySelector('.mobile');
+const mobileNav = document.querySelector('.mobile-nav');
+const mbLink = document.querySelector('.mobile__link');
 // const navLink = document.querySelectorAll('.navigation__link');
 
+const callback1 = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) mobileNav.classList.add('sticky-mobile');
+  if (entry.isIntersecting) mobileNav.classList.remove('sticky-mobile');
+};
+
+const mobileObs = new IntersectionObserver(callback1, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-90px',
+});
+
+mobileObs.observe(hero);
+//IMPLEMENTING SMOOTH SCROLL ON MOBILE DEVICES AND RESET IT
+mobileCt.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('mobile__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    mobile.style.opacity = 0;
+    mobile.style.zIndex = 0;
+    mobileNav.style.width = 0;
+    mobileNav.style.opacity = 0;
+  }
+});
+
+mbClose.addEventListener('click', function (e) {
+  mobile.style.opacity = 0;
+  mobile.style.zIndex = 0;
+  mobileNav.style.width = 0;
+  mobileNav.style.opacity = 0;
+});
+hamOpen.addEventListener('click', function (e) {
+  mobile.style.opacity = 1;
+  mobile.style.zIndex = 1000;
+  mobileNav.style.width = '25%';
+  mobileNav.style.opacity = 1;
+});
+
+//HERE WE ARE IMPLEMENTING THE SMOOTH SCROLL ON NAVIGATION NAVBAR FUNTION
 navi.addEventListener('click', function (e) {
   e.preventDefault();
   if (e.target.classList.contains('navigation__link')) {
-    console.log(e.target);
     const id = e.target.getAttribute('href');
-    console.log(id);
-    // document.querySelector(id).scrollIntoView({ behaviour: 'smooth' });
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
-// navLink.forEach(el => {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     console.log(e.target);
-//     const id = e.target.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({ behaviour: 'smooth' });
-//   });
-// });
-// console.log(navLink);
+
+priceBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute('href');
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  mobile.style.opacity = 0;
+  mobile.style.zIndex = 0;
+  mobileNav.style.width = 0;
+  mobileNav.style.opacity = 0;
+});
+
+heroDesc.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('btn')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
 //HERE WE ARE IMPLEMENTING THE STICKY NAVBAR FUNTION
-const hero = document.querySelector('.section-hero');
 const callback = function (entries) {
   const entry = entries[0];
   if (!entry.isIntersecting) headN.classList.add('sticky');
